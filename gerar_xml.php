70d26e432e56dd93310a5904743461be7491f631
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2024-12-23 17:34:03 -0300

    Update gerar_xml.php

--- gerar_xml.php
+++ gerar_xml.php
@@ -1,14 +1,9 @@
 <?php
-// Configurações do banco de dados
-$host = 'localhost';
-$dbname = 'event_database';
-$username = 'root';
-$password = '';
+require_once __DIR__ . '/config/configbd.php';
 
 try {
-    // Conectar ao banco de dados
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Conectar ao banco de dadosn  TESTE GIT
+    $pdo = Database::getConnection();
 } catch (PDOException $e) {
     die("Erro ao conectar ao banco de dados: " . $e->getMessage());
 }
