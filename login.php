b41b5a5a82f5a752d110609308b354e46aaa8b81
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2024-12-23 19:24:18 -0300

    erro 500
    Erro ao fazer login, devido ao historico

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+ini_set('log_errors', 1);
+ini_set('error_log', 'error_log.log');
+
 try {
     require_once './config/configbd.php';
     require_once './functions/scripts.php';
