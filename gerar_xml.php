2c5638868b1def2bc4328a1cbb73e69fdfba2f15
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2024-12-27 16:13:25 -0300

    Update gerar_xml.php

--- gerar_xml.php
+++ gerar_xml.php
@@ -1,4 +1,11 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+ini_set('log_errors', 1);
+ini_set('error_log', 'error_log.log');
+
 require_once __DIR__ . '/config/configbd.php';
 
 try {
