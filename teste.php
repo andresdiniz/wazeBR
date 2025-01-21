d15950997958a6c8672426c6f8113415b508d88c
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-21 16:17:15 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -9,13 +9,8 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-
-// Cabeçalhos
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Host: servicos.dnit.gov.br',
-    'Accept: */*',
     'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
 ]);
 
