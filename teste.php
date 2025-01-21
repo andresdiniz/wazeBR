ff905cacfdd6debb53f6c4f1cfb341c5d727d274
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-21 14:26:46 -0300

    Atualizar o teste.php

--- teste.php
+++ teste.php
@@ -9,58 +9,55 @@ function consultarLocalizacaoKm($longitude, $latitude, $raio = 250, $data = null
 
     file_put_contents($logFile, "Iniciando consulta\n", FILE_APPEND);
 
-    $urlBase = "https://servicos.dnit.gov.br/sgplan/apigeo/rotas/localizarkm";
-    if (!$data) {
-        $data = date('Y-m-d');
-    }
+$urlBase = "https://servicos.dnit.gov.br/sgplan/apigeo/rotas/localizarkm";
+if (!$data) {
+    $data = date('Y-m-d');
+}
 
-    $url = sprintf(
-        "%s?lng=%s&lat=%s&r=%d&data=%s",
-        $urlBase,
-        urlencode($longitude),
-        urlencode($latitude),
-        $raio,
-        urlencode($data)
-    );
+$url = sprintf(
+    "%s?lng=%s&lat=%s&r=%d&data=%s",
+    $urlBase,
+    urlencode($longitude),
+    urlencode($latitude),
+    $raio,
+    urlencode($data)
+);
 
-    file_put_contents($logFile, "URL: $url\n", FILE_APPEND);
+file_put_contents($logFile, "URL: $url\n", FILE_APPEND);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-    $response = curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-    if ($response === false) {
-        $error = curl_error($ch);
-        file_put_contents($logFile, "cURL Error: $error\n", FILE_APPEND);
-        throw new Exception('Erro ao executar a requisição: ' . $error);
-    }
+file_put_contents($logFile, "HTTP Code: $httpCode\n", FILE_APPEND);
 
-    file_put_contents($logFile, "HTTP Code: $httpCode\n", FILE_APPEND);
-    file_put_contents($logFile, "Resposta: $response\n", FILE_APPEND);
+if ($response === false) {
+    $error = curl_error($ch);
+    file_put_contents($logFile, "cURL Error: $error\n", FILE_APPEND);
+    throw new Exception('Erro ao executar a requisição: ' . $error);
+}
 
-    if ($httpCode !== 200) {
-        throw new Exception("Erro na requisição, código HTTP: $httpCode, resposta: $response");
-    }
+file_put_contents($logFile, "Resposta bruta: $response\n", FILE_APPEND);
 
-    curl_close($ch);
+curl_close($ch);
 
-    $data = json_decode($response, true);
+$data = json_decode($response, true);
 
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        $jsonError = json_last_error_msg();
-        file_put_contents($logFile, "Erro no JSON: $jsonError\n", FILE_APPEND);
-        throw new Exception("Erro ao decodificar JSON: $jsonError");
-    }
+if (json_last_error() !== JSON_ERROR_NONE) {
+    $jsonError = json_last_error_msg();
+    file_put_contents($logFile, "Erro no JSON: $jsonError\n", FILE_APPEND);
+    throw new Exception("Erro ao decodificar JSON: $jsonError");
+}
 
-    if (is_array($data) && isset($data[0]['km'])) {
-        return $data[0]['km'];
-    }
+if (is_array($data) && isset($data[0]['km'])) {
+    return $data[0]['km'];
+}
 
-    return null;
+return null;
 }
 
 // Exemplo de uso
