e3b782036ee8952710825bf409ff72c90e77a30e
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-21 16:34:14 -0300

    testes

--- teste.php
+++ teste.php
@@ -3,22 +3,33 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$url = "https://189.9.19.9/sgplan/apigeo/rotas/localizarkm?lng=-43.79944324493409&lat=-20.734276310454153&r=250&data=2025-1-21";
+$url = "https://servicos.dnit.gov.br/sgplan/apigeo/rotas/localizarkm?lng=-43.79944324493409&lat=-20.734276310454153&r=250&data=2025-1-21";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+curl_setopt($ch, CURLOPT_TIMEOUT, 120);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Accept: */*',
+    'Accept-Encoding: gzip, deflate, br',
+    'Accept-Language: pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Connection: keep-alive',
     'Host: servicos.dnit.gov.br',
+    'Referer: https://servicos.dnit.gov.br/vgeo/',
     'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
 ]);
 
 $response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
 if ($response === false) {
-    echo "Erro: " . curl_error($ch);
+    echo "Erro cURL: " . curl_error($ch) . "\n";
 } else {
-    echo "Resposta: $response";
+    echo "HTTP Code: $httpCode\n";
+    echo "Resposta: $response\n";
 }
 
 curl_close($ch);
+?>
