e0c8fce029a3d087de7fd4aaf9fd1ef7d512d7bf
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-21 15:20:25 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -3,15 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$url = "https://servicos.dnit.gov.br/sgplan/apigeo/rotas/localizarkm?lng=-43.79274845123291&lat=-20.701415185608553&r=250&data=2025-01-21";
+$url = "https://servicos.dnit.gov.br/sgplan/apigeo/rotas/localizarkm?lng=-43.79944324493409&lat=-20.734276310454153&r=250&data=2025-1-21";
 
 // Inicializa cURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Ignora verificação SSL
-curl_setopt($ch, CURLOPT_TIMEOUT, 120); // Aumenta o tempo limite
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); // Tempo de espera pela conexão
+curl_setopt($ch, CURLOPT_TIMEOUT, 120); // Aumenta o tempo limite para 2 minutos
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); // Aumenta o tempo para esperar pela conexão
 
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
