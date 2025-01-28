9051c673ef5102333ac8307961c5bc682dd8fdb9
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 14:41:11 -0300

    teste.env

--- teste.php
+++ teste.php
@@ -3,6 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+var_dump(file_exists(__DIR__ . '/../.env')); // Deve retornar true
+var_dump(realpath(__DIR__ . '/../.env'));   // Exibe o caminho absoluto do .env
+
+
 use Dotenv\Dotenv;
 
 var_dump(realpath(__DIR__ . '/../.env')); // Deve exibir o caminho absoluto do arquivo .env
