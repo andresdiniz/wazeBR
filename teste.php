bdd904c7d6a3251b5eb7329ce65b7e373ae59b9f
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 14:37:51 -0300

    .env

--- teste.php
+++ teste.php
@@ -3,13 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-
-echo getenv('EMAIL_USERNAME'); // Deve exibir: o EMAIL_USERNAME do arquivo .env
-
+use Dotenv\Dotenv;
 
 if (!file_exists(__DIR__ . '/../.env')) {
     die('Arquivo .env não encontrado!');
 }
+
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
+$dotenv->load();
