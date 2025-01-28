734f6f8ced6c25b8163ce806035ea81029297eb1
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 16:01:46 -0300

    Update scripts.php

--- functions/scripts.php
+++ functions/scripts.php
@@ -2,7 +2,7 @@
 //logs de erros
 ini_set('display_errors', 0); // Desativa a exibição de erros
 ini_set('log_errors', 1); // Ativa o registro de erros
-ini_set('error_log', __DIR__ . '/logs/error_log.php'); // Caminho do arquivo de log
+ini_set('error_log', __DIR__ . '/error_log.php'); // Caminho do arquivo de log
 
 // Definir o nível de erro que será registrado
 error_reporting(E_ALL); // Registra todos os tipos de erros
