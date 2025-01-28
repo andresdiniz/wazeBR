ed14979b68cc98b18ef31f88da625b603dd27ef5
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 14:57:38 -0300

    teste

--- teste.php
+++ teste.php
@@ -3,8 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 echo "Iniciando...\n";
 
 if (!file_exists(__DIR__ . '/.env')) {
