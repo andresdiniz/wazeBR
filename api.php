1e823cb2cbdcc7cc1549bd2d431061adc62dd21e
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 18:18:18 -0300

    Update api.php

--- api.php
+++ api.php
@@ -8,8 +8,9 @@
 //error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
 
+ini_set('display_errors', 1);   
 ini_set('log_errors', 1);
-    ini_set('error_log', __DIR__ . '/../logs/debug.log');
+ini_set('error_log', __DIR__ . '/../logs/debug.log');
 
 use Dotenv\Dotenv;
 
