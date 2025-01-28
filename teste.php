1304f9e907cb64e2de7537ed541a92582395bafc
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 14:35:09 -0300

    TESTE.ENV

--- teste.php
+++ teste.php
@@ -3,5 +3,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$output = shell_exec('wget -qO- --no-check-certificate "https://servicos.dnit.gov.br/sgplan/apigeo/rotas/localizarkm?lng=-43.79944324493409&lat=-20.734276310454153&r=250&data=2025-1-21"');
-    echo $output;
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+echo getenv('EMAIL_USERNAME'); // Deve exibir: o EMAIL_USERNAME do arquivo .env
+
+
+if (!file_exists(__DIR__ . '/../.env')) {
+    die('Arquivo .env não encontrado!');
+}
