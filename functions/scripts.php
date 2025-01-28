f4687d668edf36a01b9d033f652339742bf65707
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 16:45:43 -0300

    Update scripts.php

--- functions/scripts.php
+++ functions/scripts.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Dotenv\Dotenv;
 
 // Verificar se o arquivo .env existe
-$envPath = '/../.env';
+$envPath = '../.env';
 
 if (!file_exists($envPath)) {
     die("Arquivo .env não encontrado no caminho: $envPath");
