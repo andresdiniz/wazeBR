afa660112aa780085df8ea41814694d4990963b3
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 14:47:50 -0300

    teste.env

--- teste.php
+++ teste.php
@@ -5,12 +5,21 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+echo "Iniciando script...\n";
+
 if (!file_exists(__DIR__ . '/../.env')) {
     die('Arquivo .env não encontrado!');
 }
+echo "Arquivo .env encontrado.\n";
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/../');
 $dotenv->load();
+echo "Variáveis carregadas do .env.\n";
 
 // Testando a variável do arquivo .env
-echo getenv('EMAIL_USERNAME');
+$envVar = getenv('EMAIL_USERNAME');
+if ($envVar === false) {
+    echo "Variável EMAIL_USERNAME não encontrada no .env.\n";
+} else {
+    echo "EMAIL_USERNAME: $envVar\n";
+}
