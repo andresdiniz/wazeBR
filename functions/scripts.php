7aabb7c5915e73805439b1c0741e77915c60020a
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 20:44:34 -0300

    Update scripts.php

--- functions/scripts.php
+++ functions/scripts.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-require_once '../../config/configbd';
+require __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '../config/configbd';
 
 use Dotenv\Dotenv;
 
