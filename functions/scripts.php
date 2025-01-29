5db34f01fc3e715cff2af08c410d36a1a04f1238
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-01-28 21:38:09 -0300

    Update scripts.php

--- functions/scripts.php
+++ functions/scripts.php
@@ -405,7 +405,7 @@ function writeLog($logFilePath, $message)
 function logToFile($level, $message, $context = []) {
     // Define o caminho do log
     echo''. $level .''. $message .'';
-    $url = $logFile = __DIR__ . '/../logs/logs.log'; // Sobe um nível e acessa o diretório logs
+    $url = dirname(__DIR__) . '/logs/logs.log'; // Sobe um nível e acessa o diretório logs
     echo''. $url . PHP_EOL;
     $logFile = $url;
 
