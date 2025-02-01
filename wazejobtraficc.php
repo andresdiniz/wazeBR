0570e89943b6f39e69d211c1883c8dd862517ada
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-02-01 18:25:36 -0300

    Update wazejobtraficc.php

--- wazejobtraficc.php
+++ wazejobtraficc.php
@@ -7,7 +7,6 @@ error_reporting(E_ALL);
 set_time_limit(1200);
 require_once __DIR__ . '/config/configbd.php';
 require_once __DIR__ . '/functions/scripts.php';
-require_once __DIR__ . '/functions/email_service.php'; // Novo arquivo para e-mails
 
 $pdo = Database::getConnection();
 
