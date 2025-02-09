361e9aada9252af948f578e63da664c4e278af14
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-02-09 02:50:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -87,6 +87,8 @@ $dados = [ //Manter como dados, devido interferencia com a variavel data passada
     'pagedata' => $pageData,  // Passando o título para o template
 ];
 
+$combinedData = array_merge($dados, $data);
+
 // Renderiza os componentes fixos
 echo $twig->render('header.twig', $dados);
 echo $twig->render('sidebar.twig', $dados);
@@ -108,7 +110,7 @@ try {
     if (file_exists($controllerPath)) {
         require_once $controllerPath; // O controlador pode manipular $data
     }
-echo $twig->render($templatePath, $dados);
+echo $twig->render($templatePath, $combinedData);
     var_dump($dados);
 } catch (\Twig\Error\LoaderError $e) {
     // Renderiza página 404 caso o template não seja encontrado
