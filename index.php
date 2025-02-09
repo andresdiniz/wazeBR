5354b03e125b684669b16399c23cd261b4d87069
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-02-09 02:27:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once './config/configbd.php';
 require_once './functions/scripts.php';
 
-
 use Dotenv\Dotenv;
 
 if (!file_exists($envPath)) {
@@ -107,7 +106,7 @@ $templatePath = "{$page}.twig";
 try {
     // Carrega o controlador, se existir
     if (file_exists($controllerPath)) {
-        require_once $controllerPath; // O controlador pode manipular $data
+        echo $twig->render($templatePath, $data);
     }
 
     // Renderiza o template da página solicitada
