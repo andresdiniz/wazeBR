6e7e251a2d4e1b6f4fdcf55a5cbc7226fd5dd5d0
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-02-09 04:40:01 -0300

    Create parceiros.php

---
+++ backend/parceiros.php
@@ -0,0 +1,46 @@
+<?php
+// Inclui o arquivo de configuração do banco de dados e autoload do Composer
+require_once './config/configbd.php'; // Conexão ao banco de dados
+require_once './vendor/autoload.php'; // Autoloader do Composer
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Configura o carregador do Twig para buscar templates na pasta "frontend"
+$loader = new FilesystemLoader(__DIR__ . '/../frontend'); // Caminho para a pasta frontend
+$twig = new Environment($loader);
+
+// Conexão com o banco de dados
+$pdo = Database::getConnection();
+
+$id_parceiro = $_SESSION['usuario_id_parceiro'];
+
+// Função para buscar alertas de acidentes (ordenados pelos mais recentes)
+function geturlstraficc(PDO $pdo, $id_parceiro) {
+    $query = "
+        SELECT * 
+        FROM urls 
+        WHERE 
+    ";
+    
+    // Se não for o parceiro administrador (99), adiciona o filtro de parceiro
+    if ($id_parceiro != 99) {
+        $query .= " id_parceiro = :id_parceiro ";
+    }
+
+
+    $stmt = $pdo->prepare($query);
+
+    // Se necessário, vincula o parâmetro do parceiro
+    if ($id_parceiro != 99) {
+        $stmt->bindParam(':id_parceiro', $id_parceiro, PDO::PARAM_INT);
+    }
+
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+$data = [
+    'urls_traficc' => geturlstraficc($pdo, $id_parceiro),
+];
+
