c24878c3547e6619d3aa11d9337e77df3062dfd5
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-02-09 02:57:46 -0300

    Update sidebar.php

--- backend/sidebar.php
+++ backend/sidebar.php
@@ -1,2 +1,15 @@
-<?php 
 // Recupera o usuário logado e configurações globais
+$data = [
+    'user' => getSiteUsers($pdo, $_SESSION['usuario_id']),  // Usuário logado
+    'settings' => $settings,  // Configurações do site
+    'session' => $_SESSION,
+    'pagedata' => $pageData,  // Passando o título para o template
+];
+
+// Defina as variáveis globais
+global $globalUser, $globalSettings, $globalSession, $globalPageData;
+
+$globalUser = $data['user'];
+$globalSettings = $data['settings'];
+$globalSession = $data['session'];
+$globalPageData = $data['pagedata'];
