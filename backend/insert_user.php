285d051bbf232ad5414633c6d4c65c93cd768e27
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-02-28 13:54:36 -0300

    inserçao do php

---
+++ backend/insert_user.php
@@ -0,0 +1,99 @@
+<?php
+
+
+<?php
+// Inclui o arquivo de configuração do banco de dados e autoload do Composer
+require_once './config/configbd.php'; // Conexão ao banco de dados
+require_once './vendor/autoload.php'; // Autoloader do Composer
+require_once './functions/scripts.php'; // Funções e scripts adicionais
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+// Configura o carregador do Twig para buscar templates na pasta "frontend"
+$loader = new FilesystemLoader(__DIR__ . '/../frontend'); // Caminho para a pasta frontend
+$twig = new Environment($loader);
+
+// Conexão com o banco de dados
+$pdo = Database::getConnection();
+
+$id_parceiro = $_SESSION['usuario_id_parceiro'];
+
+// Função para buscar URLs de tráfego
+function getUrlsTraffic(PDO $pdo, $id_parceiro) {
+    $query = "
+        SELECT * 
+        FROM urls 
+    ";
+    
+    // Se não for o parceiro administrador (99), adiciona o filtro de parceiro
+    if ($id_parceiro != 99) {
+        $query .= "WHERE id_parceiro = :id_parceiro ";
+    }
+
+    $stmt = $pdo->prepare($query);
+
+    // Se necessário, vincula o parâmetro do parceiro
+    if ($id_parceiro != 99) {
+        $stmt->bindParam(':id_parceiro', $id_parceiro, PDO::PARAM_INT);
+    }
+
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getUrlsEvents(PDO $pdo, $id_parceiro) {
+    $query = "
+        SELECT * 
+        FROM urls_events 
+    ";
+    
+    // Se não for o parceiro administrador (99), adiciona o filtro de parceiro
+    if ($id_parceiro != 99) {
+        $query .= "WHERE id_parceiro = :id_parceiro ";
+    }
+
+    $stmt = $pdo->prepare($query);
+
+    // Se necessário, vincula o parâmetro do parceiro
+    if ($id_parceiro != 99) {
+        $stmt->bindParam(':id_parceiro', $id_parceiro, PDO::PARAM_INT);
+    }
+
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getUrlsAlerts(PDO $pdo, $id_parceiro) {
+    $query = "
+        SELECT * 
+        FROM urls_alerts 
+    ";
+    
+    // Se não for o parceiro administrador (99), adiciona o filtro de parceiro
+    if ($id_parceiro != 99) {
+        $query .= "WHERE id_parceiro = :id_parceiro ";
+    }
+
+    $stmt = $pdo->prepare($query);
+
+    // Se necessário, vincula o parâmetro do parceiro
+    if ($id_parceiro != 99) {
+        $stmt->bindParam(':id_parceiro', $id_parceiro, PDO::PARAM_INT);
+    }
+
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+$data = [
+    'urls_traffic' => getUrlsTraffic($pdo, $id_parceiro),
+    'urls_alerts' => getUrlsAlerts($pdo, $id_parceiro),
+    'urls_events' => getUrlsEvents($pdo, $id_parceiro),
+    'parceiros' => getParceiros($pdo, $id_parceiro),
+
+];
+
+
+
+
