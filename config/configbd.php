a07005f4ad8eac36604aa221c925cc34ca01c132
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-04-15 18:37:05 -0300

    Update configbd.php

--- config/configbd.php
+++ config/configbd.php
@@ -71,6 +71,10 @@ class Database {
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_PERSISTENT => true,
+                    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NET_READ_TIMEOUT=5, NET_WRITE_TIMEOUT=10"
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                    PDO::ATTR_STRINGIFY_FETCHES => false,   
                 ];
 
                 // Usando nossa classe monitorada
