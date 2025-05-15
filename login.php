d2fd18be904f3fdaa8ce058fbafe160d4aa853ab
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-05-15 20:32:46 -0300

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/error_log.log');
+ini_set('error_log', 'error_log.log');
 date_default_timezone_set('America/Sao_Paulo'); // Definir timezone no PHP
 
 session_start();
