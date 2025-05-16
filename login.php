4fc689c1e151af0431d96d486280eb5391e44c8e
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-05-15 21:13:48 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs/error.log');
+error_reporting(E_ALL);
+
 require_once './config/configbd.php';
 require_once './functions/scripts.php';
 
