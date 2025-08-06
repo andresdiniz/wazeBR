a8690d1c0eb46878cfc94c34cb8f606e763c96ae
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-08-06 14:44:46 -0300

    Update testes1.php

--- testes1.php
+++ testes1.php
@@ -13,8 +13,8 @@ require_once './class/class.php'; // Aqui deve estar a ApiBrasilWhatsApp
 // Dados de autenticação e destino
 $deviceToken = 'fec20e76-c481-4316-966d-c09798ae0d95';
 $authToken   = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3BsYXRhZm9ybWEuYXBpYnJhc2lsLmNvbS5ici9hdXRoL2NhbGxiYWNrIiwiaWF0IjoxNzUzMTczMzE4LCJleHAiOjE3ODQ3MDkzMTgsIm5iZiI6MTc1MzE3MzMxOCwianRpIjoia1pUMFBrWEJoRHA1Q0NPbSIsInN1YiI6Ijg1MiIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.opUGRf8f1unfjS_oJtChpoUv8Q0yYGNJChyQ8xoD5Bs';
-$numero      = '5531971408208'; // Número com DDI + DDD
-$mensagem    = 'Olá! Esta é uma mensagem automática.';
+$numero      = '5561981996661'; // Número com DDI + DDD
+$mensagem    = 'Olá! Esta é uma mensagem automática. Teste de envio via API Brasil WhatsApp.';
 
 // Instancia a classe corretamente com os tokens
 $api = new ApiBrasilWhatsApp($deviceToken, $authToken);
