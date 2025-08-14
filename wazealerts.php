8fe7c9f65e48628c4439251ccf34ba2927a5b6a6
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-08-14 17:46:59 -0300

    Update wazealerts.php

--- wazealerts.php
+++ wazealerts.php
@@ -149,7 +149,7 @@ function saveAlertsToDb(PDO $pdo, array $alerts, $url, $id_parceiro)
             status = 1, date_updated = VALUES(date_updated), km = VALUES(km), id_parceiro = VALUES(id_parceiro)");
 
         $incomingUuids = [];
-        $DUPLICATE_DISTANCE_THRESHOLD = 1500;
+        $DUPLICATE_DISTANCE_THRESHOLD = 1200;
 
         foreach ($alerts as $alert) {
             if (!isset($alert['location']['x'], $alert['location']['y'])) {
