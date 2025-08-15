edab9bbec744eeb7222057e47554b03d93fcf635
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-08-14 21:08:12 -0300

    Update worker_notifications.php

--- worker_notifications.php
+++ worker_notifications.php
@@ -64,7 +64,7 @@ try {
                 $deviceToken = 'fec20e76-c481-4316-966d-c09798ae0d95';
                         $authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3BsYXRhZm9ybWEuYXBpYnJhc2lsLmNvbS5ici9hdXRoL2NhbGxiYWNrIiwiaWF0IjoxNzUzMTczMzE4LCJleHAiOjE3ODQ3MDkzMTgsIm5iZiI6MTc1MzE3MzMxOCwianRpIjoia1pUMFBrWEJoRHA1Q0NPbSIsInN1YiI6Ijg1MiIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.opUGRf8f1unfjS_oJtChpoUv8Q0yYGNJChyQ8xoD5Bs';
                         // Lista de números para enviar
-                enviarNotificacaoWhatsApp($pdo, $deviceToken, $authToken, [$envio['phone']], $envio['uuid_alerta']);
+                enviarNotificacaoWhatsApp($pdo, $deviceToken, $authToken, [$envio['phone']], $envio['id']);
                 $message = "Mensagem enviada para " . $envio['phone'];
             } else {
                 $message = "Nenhum contato disponível";
@@ -113,7 +113,7 @@ try {
             ':data_envio' => $dataAtualizacao,
             ':mensagem_erro' => ($status === 'ERRO') ? $message : null,
             ':enviado' => ($status === 'ENVIADO') ? 1 : 0,
-            ':uuid_alerta' => $envio['uuid_alerta']
+            ':uuid_alerta' => $envio['id']
         ]);
 
         // Log do envio
