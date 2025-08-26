6bd6441463d0c43edfe60d61209f7a16bcbc838f
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-08-26 17:45:05 -0300

    Update wazejobtraficc.php

--- wazejobtraficc.php
+++ wazejobtraficc.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-set_time_limit(1200);
+set_time_limit(0.500);
 
 require_once __DIR__ . '/config/configbd.php';
 require_once __DIR__ . '/functions/scripts.php';
