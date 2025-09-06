ea54c267cd676ea8b4cc1396e24f928924e37c6d
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2025-09-06 19:18:05 -0300

    Update dadoscemadem.php

--- dadoscemadem.php
+++ dadoscemadem.php
@@ -55,7 +55,7 @@ try {
     $dados = obterDadosCemaden($url);
 
     var_dump($dados);
-    //echo "o nivel atual é $dados['nivel_atual'];
+    echo "O nivel atual é $item['valor'];
 
     // Prepara a inserção no banco de dados
     $stmt = $pdo->prepare(" 
